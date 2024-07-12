9d4b1f0c0c8f59acd2401b11cc649a95243e5596
Nadiia Sokol <dianadin@ukr.net>
2024-07-12 20:25:38 +0200

    js

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+'use strict';
+const makeArray = function(firstArray, secondArray, maxLength){
+    const newMakeArr = firstArray.concat(secondArray);
+    if(newMakeArr.length = maxLength) { 
+        const length = newMakeArr.slice();
+        return newMakeArr;
+    }
+        else {
+        const length = newMakeArr;
+        return length;
+        }
+    
+
+}
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
